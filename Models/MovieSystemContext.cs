0ad8870dab5484434875ce61b1245926a63b217c
HagerMagdy2 <hager9671@gmail.com>
2024-12-27 00:40:34 +0200

    DbSets

--- Models/MovieSystemContext.cs
+++ Models/MovieSystemContext.cs
@@ -14,6 +14,15 @@ namespace MovieReservationSystemAPI.Models
         public virtual DbSet<Movie> Movies { get; set; }
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Admin> Admins { get; set; }
+        public virtual DbSet<Actor> Actors { get; set; }
+        public virtual DbSet<Hall> Halls { get; set; }
+        public virtual DbSet<Reservation> Reservations { get; set; }
+        public virtual DbSet<ResevationDetails> ResevationDetail { get; set; }
+        public virtual DbSet<Seat> Seats { get; set; }
+        public virtual DbSet<ShowTime> ShowsTime { get; set; }
+        public virtual DbSet<Types> Types { get; set; }
+
+
 
         //protected override void OnModelCreating(ModelBuilder builder)
         //{
